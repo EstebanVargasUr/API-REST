cc38b915e3ab889ef060fab0348fad6c189ba1f4
Gurreck <daniel.gurreck.gonzalez@est.una.ac.cr>
2020-08-29 22:22:24 -0600

    Se implemento Tramites_Tipo

---
+++ tramites/src/main/java/org/una/tramites/entities/Tramite_Tipo.java
@@ -0,0 +1,69 @@
+package org.una.tramites.entities;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "tramites_tipos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Tramite_Tipo implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "descripcion", length = 100)
+    private String descripcion;
+    
+    @Column
+    private boolean estado;
+
+    @Column(name = "fecha_registro", updatable = false)
+    @Temporal(TemporalType.DATE)
+    @Setter(AccessLevel.NONE)
+    private Date fechaRegistro;
+
+    @Column(name = "fecha_modificacion")
+    @Setter(AccessLevel.NONE)
+    @Temporal(TemporalType.DATE)
+    private Date fechaModificacion;
+    
+    @ManyToOne 
+    @JoinColumn(name="departamentos_id")
+    private Departamento departamento;
+    
+    private static final long serialVersionUID = 1L;
+
+    @PrePersist
+    public void prePersist() {
+        estado=true;
+        fechaRegistro = new Date();
+        fechaModificacion = new Date();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        fechaModificacion = new Date();
+    }
+}
+
