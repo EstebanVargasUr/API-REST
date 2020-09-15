79b65a53d7328553831ef6dba795cffbc0bc18e8
Esteban Vargas Ureña <estebanvargasu@gmail.com>
2020-09-14 21:26:43 -0600

    Se elimino JavaFX XD

--- tramites/src/main/java/org/una/tramites/TramitesApplication.java
+++ tramites/src/main/java/org/una/tramites/TramitesApplication.java
@@ -1,32 +1,16 @@
 package org.una.tramites;
 
-import javafx.application.Application;
-import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2 
-public class TramitesApplication extends Application{
+public class TramitesApplication{
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("view/Dashboard.fxml"));
-        
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-       
-        stage.show();
-        
-    }
     public static void main(String[] args) {
        SpringApplication.run(TramitesApplication.class, args);
-       launch(); 
         
     }
 
