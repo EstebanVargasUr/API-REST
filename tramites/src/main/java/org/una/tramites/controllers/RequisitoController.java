fc81fef192998c9af2f4e99faa6025529f14a366
Adrian Gamboa Delgado <norman.gamboa.delgado@est.una.ac.cr>
2020-09-25 14:25:49 -0600

    Mejoras en: requisito y requisito presentado

--- tramites/src/main/java/org/una/tramites/controllers/RequisitoController.java
+++ tramites/src/main/java/org/una/tramites/controllers/RequisitoController.java
@@ -3,12 +3,13 @@ package org.una.tramites.controllers;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import java.util.Date;
-import java.util.List;
 import java.util.Optional;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,10 +18,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.una.tramites.dto.RequisitoDTO;
-import org.una.tramites.entities.Requisito;
 import org.una.tramites.services.IRequisitoService;
 import org.una.tramites.utils.MapperUtils;
 
@@ -35,142 +34,122 @@ public class RequisitoController {
     
     @Autowired
     private IRequisitoService requisitoService;
+    
+    final String MENSAJE_VERIFICAR_INFORMACION = "Debe verifiar el formato y la información de su solicitud con el formato esperado";
 
-    @GetMapping() 
-    @ApiOperation(value = "Obtiene una lista de todas los Requisitos", response = RequisitoDTO.class, responseContainer = "List", tags = "Requisitos")
+    @GetMapping("/")
+    @ApiOperation(value = "Obtiene una lista de todos los requisitos", response = RequisitoDTO.class, responseContainer = "List", tags = "Requisitos")
     @PreAuthorize("hasAuthority('REQUISITO_CONSULTAR_TODO')")
-    public @ResponseBody
-    ResponseEntity<?> findAll() {
+    public ResponseEntity<?> findAll() {
         try {
-            Optional<List<RequisitoDTO>> result = requisitoService.findAll();
-            if (result.isPresent()) {
-                List<RequisitoDTO> requisitoDTO = MapperUtils.DtoListFromEntityList(result.get(), RequisitoDTO.class);
-                return new ResponseEntity<>(requisitoDTO, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
+            return new ResponseEntity(requisitoService.findAll(), HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity(e.getClass(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @GetMapping("/{id}") 
-    @ApiOperation(value = "Obtiene una lista con el requisito por medio del Id", response = RequisitoDTO.class, responseContainer = "List", tags = "Requisitos")
+    @GetMapping("/{id}")
     @PreAuthorize("hasAuthority('REQUISITO_CONSULTAR')")
+    @ApiOperation(value = "Obtiene un requisito por su Id", response = RequisitoDTO.class, tags = "Requisitos")
     public ResponseEntity<?> findById(@PathVariable(value = "id") Long id) {
         try {
-
-            Optional<RequisitoDTO> requisitoFound = requisitoService.findById(id);
-            if (requisitoFound.isPresent()) {
-                RequisitoDTO variacionDto = MapperUtils.DtoFromEntity(requisitoFound.get(), RequisitoDTO.class);
-                return new ResponseEntity<>(variacionDto, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
+            return new ResponseEntity(requisitoService.findById(id), HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
     
-    @GetMapping("/{estado}") 
-    @ApiOperation(value = "Obtiene una lista de los Requisitos por estado", response = RequisitoDTO.class, responseContainer = "List", tags = "Requisitos")
-    @ResponseBody
+    @GetMapping("/{estado}")
     @PreAuthorize("hasAuthority('REQUISITO_CONSULTAR')")
-    public ResponseEntity<?> findByEstado(@PathVariable(value = "estado") boolean estado){
+    @ApiOperation(value = "Obtiene un requisito por su estado", response = RequisitoDTO.class, tags = "Requisitos")
+    public ResponseEntity<?> findByEstado(@PathVariable(value = "estado") boolean estado) {
         try {
-            Optional<List<RequisitoDTO>> result = requisitoService.findByEstado(estado);
-            if (result.isPresent()) {
-                List<RequisitoDTO> requisitoDTO = MapperUtils.DtoListFromEntityList(result.get(), RequisitoDTO.class);
-                return new ResponseEntity<>(requisitoDTO, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
+            return new ResponseEntity(requisitoService.findByEstado(estado), HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
     
-    @GetMapping("/{fecha}") 
-    @ApiOperation(value = "Obtiene una lista de Requisitos entre la fecha especificada", response = RequisitoDTO.class, responseContainer = "List", tags = "Requisitos")
+    @GetMapping("/{fecha}")
     @PreAuthorize("hasAuthority('REQUISITO_CONSULTAR')")
-    public @ResponseBody
-    ResponseEntity<?> findByFechaRegistroBetween(@PathVariable(value = "Fecha inicial") Date startDate, @PathVariable(value = "Fecha final") Date endDate) {
+    @ApiOperation(value = "Obtiene un requisito entre fechas", response = RequisitoDTO.class, tags = "Requisitos")
+    public ResponseEntity<?> findByFechaRegistroBetween(@PathVariable(value = "fecha") Date fecha1, Date fecha2) {
         try {
-            Optional<List<RequisitoDTO>> result = requisitoService.findByFechaRegistroBetween(startDate, endDate);
-            if (result.isPresent()) {
-                List<RequisitoDTO> fechaDTO = MapperUtils.DtoListFromEntityList(result.get(), RequisitoDTO.class);
-                return new ResponseEntity<>(fechaDTO, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
+            return new ResponseEntity(requisitoService.findByFechaRegistroBetween(fecha1, fecha2), HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @GetMapping("/variaciones/{id}")
-    @ApiOperation(value = "Obtiene una lista con los Requisitos por Variación", response = RequisitoDTO.class, responseContainer = "List", tags = "Requisitos")
-    @PreAuthorize("hasAuthority('REQUISITO_CONSULTAR')")
-    public ResponseEntity<?> findByTramite_tipoId(@PathVariable(value = "term") long term) {
-        try {
-            Optional<List<RequisitoDTO>> result = requisitoService.findByVariacionId(term);
-            if (result.isPresent()) {
-                List<RequisitoDTO> requisitoDto = MapperUtils.DtoListFromEntityList(result.get(), RequisitoDTO.class);
-                return new ResponseEntity<>(requisitoDto, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
     
-    @ResponseStatus(HttpStatus.OK)
-    @PostMapping("/") 
-    @ResponseBody
-    @ApiOperation(value = "Permite crear un Requisito", response = RequisitoDTO.class, tags = "Requisitos")
+    @GetMapping("/{Variacion}")
     @PreAuthorize("hasAuthority('REQUISITO_CONSULTAR')")
-    public ResponseEntity<?> create(@RequestBody RequisitoDTO requisito) {
+    @ApiOperation(value = "Obtiene un requisito por medio del id de la variacion", response = RequisitoDTO.class, tags = "Requisitos")
+    public ResponseEntity<?> findByVariacionId(@PathVariable(value = "id") long id) {
         try {
-            RequisitoDTO variacionCreated = requisitoService.create(requisito);
-            RequisitoDTO requisitoDto = MapperUtils.DtoFromEntity(variacionCreated, RequisitoDTO.class);
-            return new ResponseEntity<>(requisitoDto, HttpStatus.CREATED);
+            return new ResponseEntity(requisitoService.findByVariacionId(id), HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
-    @PutMapping("/{id}") 
-    @ResponseBody
-    @ApiOperation(value = "Permite modificar un Requisito a partir de su Id", response = RequisitoDTO.class, tags = "Requisitos")
-    @PreAuthorize("hasAuthority('REQUISITO_CONSULTAR')")
-    public ResponseEntity<?> update(@PathVariable(value = "id") Long id, @RequestBody RequisitoDTO requisitoModified) {
-        try {
-            Optional<RequisitoDTO> requisitoUpdated = requisitoService.update(requisitoModified, id);
-            if (requisitoUpdated.isPresent()) {
-                RequisitoDTO variacionDto = MapperUtils.DtoFromEntity(requisitoUpdated.get(), RequisitoDTO.class);
-                return new ResponseEntity<>(variacionDto, HttpStatus.OK);
-
-            } else {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-
+    
+    @PostMapping("/")
+    @ApiOperation(value = "Permite crear un requisito", response = RequisitoDTO.class, tags = "Requisitos")
+    @PreAuthorize("hasAuthority('USUARIO_CREAR')")
+    public ResponseEntity<?> create(@Valid @RequestBody RequisitoDTO requisitoDTO, BindingResult bindingResult) {
+        if (!bindingResult.hasErrors()) {
+            try {
+                return new ResponseEntity(requisitoService.create(requisitoDTO), HttpStatus.CREATED);
+            } catch (Exception e) {
+                return new ResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
             }
-        } catch (Exception e) {
-            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        } else {
+            return new ResponseEntity(MENSAJE_VERIFICAR_INFORMACION, HttpStatus.BAD_REQUEST);
         }
     }
 
-    @DeleteMapping("/{id}") 
+
+    @PutMapping("/{id}")
+    @ApiOperation(value = "Permite modificar un requisito a partir de su Id", response = RequisitoDTO.class, tags = "Requisitos")
+    @PreAuthorize("hasAuthority('USUARIO_MODIFICAR')")
+    public ResponseEntity<?> update(@PathVariable(value = "id") Long id, @Valid @RequestBody RequisitoDTO requisitoDTO, BindingResult bindingResult) {
+        if (!bindingResult.hasErrors()) {
+            try {
+                Optional<RequisitoDTO> requisitoUpdated = requisitoService.update(requisitoDTO, id);
+                if (requisitoUpdated.isPresent()) {
+                    return new ResponseEntity(requisitoUpdated, HttpStatus.OK);
+                } else {
+                    return new ResponseEntity(HttpStatus.NOT_FOUND);
+                }
+            } catch (Exception e) {
+                return new ResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            return new ResponseEntity(MENSAJE_VERIFICAR_INFORMACION, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "Permite eliminar un requisito a partir de su Id", response = RequisitoDTO.class, tags = "Requisitos")
     @PreAuthorize("hasAuthority('REQUISITO_ELIMINAR')")
     public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
-        return null;
-        //TODO: Implementar este método
+        try {
+            requisitoService.delete(id);
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @DeleteMapping("/") 
+    @DeleteMapping("/")
+    @ApiOperation(value = "Permite eliminar todos los requisito", response = RequisitoDTO.class, tags = "Requisitos")
     @PreAuthorize("hasAuthority('REQUISITO_ELIMINAR_TODO')")
     public ResponseEntity<?> deleteAll() {
-        return null;
- 	//TODO: Implementar este método
-    } 
+        try {
+            requisitoService.deleteAll();
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- tramites/src/main/java/org/una/tramites/services/RequisitoServiceImplementation.java
+++ tramites/src/main/java/org/una/tramites/services/RequisitoServiceImplementation.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.una.tramites.dto.RequisitoDTO;
 import org.una.tramites.entities.Requisito;
 import org.una.tramites.repositories.IRequisitoRepository;
+import org.una.tramites.utils.MapperUtils;
 
 /**
  *
@@ -19,56 +20,84 @@ public class RequisitoServiceImplementation implements IRequisitoService{
 
     @Autowired
     private IRequisitoRepository requisitoRepository;
+    
+    private Optional<List<RequisitoDTO>> findList(List<Requisito> list) {
+        if (list != null) {
+            List<RequisitoDTO> usuariosDTO = MapperUtils.DtoListFromEntityList(list, RequisitoDTO.class);
+            return Optional.ofNullable(usuariosDTO);
+        } else {
+            return null;
+        }
+    }
 
+    private Optional<List<RequisitoDTO>> findList(Optional<List<Requisito>> list) {
+        if (list.isPresent()) {
+            return findList(list.get());
+        } else {
+            return null;
+        }
+    }
+
+    private Optional<RequisitoDTO> oneToDto(Optional<Requisito> one) {
+        if (one.isPresent()) {
+            RequisitoDTO requisitoDTO = MapperUtils.DtoFromEntity(one.get(), RequisitoDTO.class);
+            return Optional.ofNullable(requisitoDTO);
+        } else {
+            return null;
+        }
+    }
+    
     @Override
     @Transactional(readOnly = true)
     public Optional<List<RequisitoDTO>> findAll() {
-        return Optional.ofNullable(requisitoRepository.findAll());
+        return findList(requisitoRepository.findAll());
     }
 
     @Override
     @Transactional(readOnly = true)
     public Optional<RequisitoDTO> findById(Long id) {
-        return requisitoRepository.findById(id);
+        return oneToDto(requisitoRepository.findById(id));
     }
     
     @Override
     @Transactional(readOnly = true)
     public Optional<List<RequisitoDTO>> findByVariacionId(Long id) {
-        return Optional.ofNullable(requisitoRepository.findByVariacionId(id));
+        return findList(requisitoRepository.findByVariacionId(id));
     }
     
     @Override
     public Optional<List<RequisitoDTO>> findByEstado(boolean estado) {
-        return requisitoRepository.findByEstado(estado);
+        return findList(requisitoRepository.findByEstado(estado));
     }
     
     @Override
     public Optional<List<RequisitoDTO>> findByFechaRegistroBetween(Date startDate, Date endDate) {
-        return requisitoRepository.findByFechaRegistroBetween(startDate, endDate);
+        return findList(requisitoRepository.findByFechaRegistroBetween(startDate, endDate));
     }
     
     @Override
     @Transactional
-    public RequisitoDTO create(RequisitoDTO requisito) {
-        return requisitoRepository.save(requisito);
+    public RequisitoDTO create(RequisitoDTO requisitoDTO) {
+        Requisito requisito = MapperUtils.EntityFromDto(requisitoDTO, Requisito.class);
+        requisito = requisitoRepository.save(requisito);
+        return MapperUtils.DtoFromEntity(requisito, RequisitoDTO.class);
     }
 
     @Override
     @Transactional
-    public Optional<RequisitoDTO> update(RequisitoDTO requisito, Long id) {
+    public Optional<RequisitoDTO> update(RequisitoDTO requisitoDTO, Long id) {
         if (requisitoRepository.findById(id).isPresent()) {
-            return Optional.ofNullable(requisitoRepository.save(requisito));
+            Requisito requisito = MapperUtils.EntityFromDto(requisitoDTO, Requisito.class);
+            requisito = requisitoRepository.save(requisito);
+            return Optional.ofNullable(MapperUtils.DtoFromEntity(requisito, RequisitoDTO.class));
         } else {
             return null;
-        }
-
+        } 
     }
 
     @Override
     @Transactional
     public void delete(Long id) {
-
         requisitoRepository.deleteById(id);
     }
 
